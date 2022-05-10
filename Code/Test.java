586c6a8c1e10035dc74745f1de866a3f0c3402a9
LordKzoth <nik-kron@mail.ru>
2022-05-10 03:35:36 +0300

    LeetCode Task

--- Code/Test.java
+++ Code/Test.java
@@ -1,5 +1,30 @@
 class Test {
     public static void main(String[] args) {
-        System.out.println("Hello, Dude!");
+        System.out.println(lengthOfLongestSubstring("abcabcbb"));
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+        int max = 0;
+        
+        String currentString = "";
+        for (int i = 0; i < s.length(); ++i) {
+            char currentChar = s.charAt(i);
+
+            if (currentString.contains(Character.toString(currentChar))) {
+                if (currentString.length() > max) {
+                    max = currentString.length();
+                }
+
+                currentString = currentString.substring(currentString.indexOf(currentChar) + 1) + currentChar;
+            } else {
+                currentString = currentString + currentChar;
+            }
+        }
+
+        if (currentString.length() > max) {
+            max = currentString.length();
+        }
+        
+        return max;
     }
 }
